9a2c6f770dc3c490e9f810cd3e8d88877a8719ab
evelyn-epprecht <evelynrib2306@gmail.com>
2023-02-14 21:22:42 -0300

    Versão semi final

--- js/script.js
+++ js/script.js
@@ -13,7 +13,17 @@ menu.onclick = () => {
     navlist.classList.toggle("active");
 }
 
-menu.onscroll = () => {
+window.onscroll = () => {
     menu.classList.remove("bx-x");
     navlist.classList.remove("active");
 }
+
+const sr = ScrollReveal ({
+    distance:'45px',
+    duration:2700,
+    reset: true
+})
+
+sr.reveal('.home-text', {delay:350, origin:'left'})
+sr.reveal('.home-img', {delay:350, origin:'right'})
+sr.reveal('.sub-service, .about, .portfolio,.service,.cta, .contact', {delay:200, origin:'bottom'})
